21684f12551ef305b52bfec71e07c58c57f6d2d1
ravikandekar <153173943+ravikandekar@users.noreply.github.com>
2024-08-27 18:51:33 +0530

    vendor profile working

---
+++ src/ReusableComponents/CustomButtonNew.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { TouchableOpacity, View, Text, StyleSheet, Image } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import CustomColors from '../styles/CustomColors';
+
+const CustomButtonNew = ({
+  text,
+  rightIcon = false,
+  leftIconPath = null,
+  buttonBgColor = CustomColors.mattBrownDark,
+  rightIconBgColor = CustomColors.accentGreen,
+  leftIconBgColor = CustomColors.accentGreen,
+  onPress,
+  textSize = 16,
+  fontWeight = 'bold',
+  paddingHorizontal = wp(2),
+  paddingVertical = wp(2),
+  ...rest
+}) => {
+  return (
+    <TouchableOpacity style={[styles.button, { backgroundColor: buttonBgColor }, rest]} onPress={onPress}>
+        <View style={styles.container}>
+        {leftIconPath && (
+          <View style={[styles.iconContainer, { backgroundColor: leftIconBgColor }]}>
+            <Image source={leftIconPath} style={styles.leftIcon} size={50} />
+          </View>
+        )}
+        <Text style={[styles.text, { fontSize: textSize, fontWeight: fontWeight, paddingHorizontal: paddingHorizontal, paddingVertical: paddingVertical }]}>{text}</Text>
+        {rightIcon && (
+          <View style={[styles.iconContainer, { backgroundColor: rightIconBgColor }]}>
+            <Image source={rightIcon} style={styles.leftIcon} size={50} />
+          </View>
+        )}
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 50,
+    elevation: 5,
+    alignSelf: 'flex-start', // This will make the button wrap its content
+  },
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    textAlign: 'center',
+    fontFamily:'Poppins-Bold',
+    paddingHorizontal: wp(5),
+    paddingVertical: wp(1),
+  },
+  iconContainer: {
+    width: 35,
+    height: 35,
+    borderRadius: 50,
+    margin: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  leftIcon: {
+    width: 35,
+    height: 35,
+    resizeMode: 'contain',
+  },
+});
+
+export default CustomButtonNew;
---
+++ src/ReusableComponents/ReviewsItem.js
@@ -0,0 +1,69 @@
+import { StyleSheet, View, Text, Image } from "react-native"
+import { Rating } from "react-native-ratings"
+import { widthPercentageToDP as wp } from "react-native-responsive-screen"
+
+const ReviewsItem =({reviewItem})=>{
+    return(
+        <View style={styles.container}>
+            <View style={styles.contentContainer}>
+                <View style={styles.rowStyle}>
+                    <Text style={styles.headingStyle} ellipsizeMode="tail" numberOfLines={1}>{reviewItem.name}</Text>
+                    <Rating imageSize={wp(5)} readonly={true} startingValue={reviewItem.rating}/>
+                </View>
+                <Text ellipsizeMode="tail" numberOfLines={3}>{reviewItem.description}</Text>
+            </View>
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={reviewItem.imagePath}/>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        padding: wp(2),
+        width: wp(95),
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+
+    },
+    contentContainer:{
+        elevation: 5,
+        borderRadius: wp(25),
+        width: '90%',
+        paddingStart: wp(14),
+        paddingVertical: wp(1),
+        backgroundColor: 'white',
+        alignItems: 'flex-start',
+        paddingBottom: wp(4)
+    },
+    rowStyle:{
+        flexDirection: 'row',
+        marginBottom: wp(2),
+        marginTop: wp(6),
+        alignItems: 'center'
+    },
+    headingStyle:{
+        color: '#000',
+        fontSize: wp(5),
+        fontWeight: 'bold',
+        marginRight: wp(3)
+    },
+    imageContainer:{
+        elevation: 5,
+        marginStart: wp(2),
+        borderRadius: wp(25),
+        height: wp(22),
+        width: wp(22),
+        position: 'absolute',
+        backgroundColor: 'blue',
+        top: 0, left: 0,
+    },
+    image:{
+        height: wp(22),
+        width: wp(22),
+        borderRadius: wp(25),
+    },
+})
+
+export default ReviewsItem;
